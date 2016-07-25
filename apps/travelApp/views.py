996c95212e253a20f5dfb0589696c332dbae0da0
Curran Brown <Currbear@Currans-MBP.hsd1.wa.comcast.net>
2016-07-25 16:37:16 -0700

    first commit with login and registration HTML started

---
+++ apps/travelApp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+
+	return render(request, 'travelAppTemplates/index.html')
+
+def register(request):
+
+	return render(request, 'travelAppTemplates/register.html')
